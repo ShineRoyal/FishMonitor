25b4d3baeb718ec769f0776286fef45e79e88952
ShineRoyal <1050799608@qq.com>
2020-04-17 15:31:05 +0800

    ds18b20 ok

--- USER/main.c
+++ USER/main.c
@@ -6,7 +6,7 @@
 #include "key.h"
 
 #include "w25qxx.h"
-
+#include "ds18b20.h"
 #include "usart3.h"
 #include "common.h"
 
@@ -23,7 +23,7 @@
 
 int main(void)
 {
-
+    short temperature;
 
     delay_init();            //延时函数初始化
     NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);//设置中断优先级分组为组2：2位抢占优先级，2位响应优先级
@@ -32,14 +32,25 @@ int main(void)
     KEY_Init();                 //初始化按键
     W25QXX_Init();              //初始化W25Q128
     usart3_init(115200);        //初始化串口3
-	
+    while (DS18B20_Init())  //DS18B20初始化
+    {
+        printf("DS18B20 Error\r\n");
+        delay_ms(500);
+    }
+    temperature = DS18B20_Get_Temp();
+    while (1)
+    {
+        temperature = DS18B20_Get_Temp();
+        printf("%d.%dC\r\n", temperature / 10, temperature % 10);
+        delay_ms(500);
+    }
     printf("init ok\r\n");
     while (atk_8266_send_cmd("AT", "OK", 20)) //检查WIFI模块是否在线
     {
         atk_8266_quit_trans();//退出透传
         atk_8266_send_cmd("AT+CIPMODE=0", "OK", 200); //关闭透传模式
         delay_ms(800);
-				printf("no 8266\r\n");
+        printf("no 8266\r\n");
 
     }
     while (atk_8266_send_cmd("ATE0", "OK", 20)); //关闭回显
